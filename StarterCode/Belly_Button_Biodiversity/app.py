cbe1c63f755120273accb1a5732f33558188e23d
Chol Gurung <chol.gurung@gmail.com>
2018-11-08 01:29:06 -0800

    final changes to interactive chart

--- StarterCode/Belly_Button_Biodiversity/app.py
+++ StarterCode/Belly_Button_Biodiversity/app.py
@@ -97,21 +97,20 @@ def samples(sample):
     return jsonify(data)
 
 @app.route("/wfreq/<sample>")
-def samples(sample):
-    """Return `otu_ids`, `otu_labels`,and `sample_values`."""
-    stmt = db.session.query(Samples).statement
-    df = pd.read_sql_query(stmt, db.session.bind)
+def samples_gauge(sample):
+    sel = [
+        Samples_Metadata.WFREQ
+    ]
 
-    # Filter the data based on the sample number and
-    # only keep rows with values above 1
-    sample_data = df.loc[df[sample] > 1, ["otu_id", "otu_label", sample]]
-    # Format the data to send as json
-    data = {
-        "otu_ids": sample_data.otu_id.values.tolist(),
-        "sample_values": sample_data[sample].values.tolist(),
-        "otu_labels": sample_data.otu_label.tolist(),
-    }
-    return jsonify(data)
+    results = db.session.query(*sel).filter(Samples_Metadata.sample == sample).all()
+
+    # Create a dictionary entry for each row of metadata information
+    sample_wfreqdata = {}
+    for result in results:
+        sample_wfreqdata["WFREQ"] = result[0]
+
+    print(sample_wfreqdata)
+    return jsonify(sample_wfreqdata)
 
 if __name__ == "__main__":
     app.run()
